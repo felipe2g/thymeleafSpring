4859d35fbed74e4c46966290fcfc0c2168389d89
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-10-07 22:23:18 -0300

    add change professor

--- src/main/java/com/example/thymeleafspring/Professor/ProfessorController.java
+++ src/main/java/com/example/thymeleafspring/Professor/ProfessorController.java
@@ -40,4 +40,30 @@ public class ProfessorController {
         jdbc.update("delete from professor where id = ?", cod);
         return "redirect:/professor";
     }
+
+    @GetMapping("update")
+    public String getUpdateProfessor(@RequestParam(value = "id", required = true) Integer cod, Model model) {
+        Professor professor = jdbc.queryForObject("select * from professor where id = ?", (rs, rowNum) -> {
+            Professor p = new Professor();
+            p.setId(rs.getLong("id"));
+            p.setNome(rs.getString("nome"));
+
+            return p;
+        }, cod);
+
+        model.addAttribute("professor", professor);
+
+        return "professor/updateProfessor";
+    }
+
+    @PostMapping("updateProfessor")
+    public String postUpdateProfessor(Professor professor) {
+        System.out.println("professor.getnome => " + professor.getNome());
+        System.out.println("professor.getId => " + professor.getId());
+        jdbc.update("UPDATE professor set nome = ? where id = ?",
+                professor.getNome(),
+                professor.getId());
+
+        return "redirect:/professor";
+    }
 }
