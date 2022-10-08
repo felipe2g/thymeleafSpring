7d6f0d6058d0d19bb155dd69a1539addcdde12ab
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-10-07 21:26:00 -0300

    add read to professor routes

--- src/main/java/com/example/thymeleafspring/Professor/ProfessorController.java
+++ src/main/java/com/example/thymeleafspring/Professor/ProfessorController.java
@@ -3,8 +3,11 @@ package com.example.thymeleafspring.Professor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("professor")
@@ -13,7 +16,10 @@ public class ProfessorController {
     JdbcTemplate jdbc;
 
     @GetMapping("")
-    public String getProfessor() {
+    public String getProfessor(Model model) {
+        List<Professor> professores = jdbc.query("SELECT id, nome from professor", (rs, rowNum) -> new Professor(rs.getLong("id"), rs.getString("nome")));
+        model.addAttribute("professores", professores);
+
         return "professor";
     }
 }
